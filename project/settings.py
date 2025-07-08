d92ad9f966eb2c2af27f60718fc8a9105b920fc7
ganeshborkar08 <ganesh.borkar@consociatesolutions.com>
2025-07-08 17:32:05 +0530

    add static files

--- project/settings.py
+++ project/settings.py
@@ -118,7 +118,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_ROOT = '/home/ubuntu/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
