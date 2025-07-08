71fb782482d4fee203018c7cb7655229cf29b1d2
ganeshborkar08 <ganesh.borkar@consociatesolutions.com>
2025-07-08 17:50:49 +0530

    minor changes

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://cspldjango.corniya.com"
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
